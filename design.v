cecc4e0c5ed01e372640f907c4aa73bd830bc1c9
Kintali Saicharan <92353250+saicharan0426@users.noreply.github.com>
2024-08-29 23:51:39 +0530

    Add files via upload

---
+++ design.v
@@ -0,0 +1,35 @@
+// Code your design here
+// Code your design here
+`timescale 1ns/10ps
+module memory (DataBus, MemWr, MemRd, Addr);
+  inout [63:0] DataBus;
+  input MemWr, MemRd;
+  input [5:0] Addr;
+  reg [63:0] datareg;
+  reg [63:0] Mem [0:63];//memory declaration
+ 
+  initial begin
+    $readmemh ("input.txt", Mem);
+    $display($time,"ns data write process done ");
+  end
+  
+  always @ (MemWr or MemRd or Addr or datareg) begin
+    	if (MemWr==1'b1 && MemRd==1'b0) begin
+				Mem [Addr] =DataBus;
+				//datareg=64'hzzzzzzzzzzzzzzzz;
+          		datareg =64'h1111111111111111;
+	 	end
+	 	else if (MemRd==1'b1)	begin
+        		datareg= Mem[Addr];
+  
+          $display($time,"ns data read Address-%h is %h\n",
+                   Addr,datareg);
+ 
+          $display($time,"ns data bus data is %h\n",DataBus);
+	 	end
+	 	else
+			datareg=64'hzzzzzzzzzzzzzzzz;
+   end
+  assign DataBus = datareg;
+  
+endmodule
---
+++ testbench.v
@@ -0,0 +1,70 @@
+// Code your testbench here
+// or browse Examples
+
+// Code your testbench here
+// or browse Examples
+module tb;
+	reg MemWr;
+	reg MemRd;
+	reg [5:0] Addr;
+	wire [63:0] DataBus;
+	reg [63:0] Mem [0:63];
+// Instantiate the Unit Under Test (UUT)
+memory uut (
+		.DataBus(DataBus),
+  		.MemWr(MemWr),
+  		.MemRd(MemRd),
+		.Addr(Addr)
+	 );
+
+  initial begin
+// Initialize Inputs
+		MemWr = 0;
+		MemRd = 0;
+		Addr = 0;
+// Wait 100 ns for global reset to finish
+		#20;
+		MemRd = 1;
+		MemWr = 0;
+		Addr = 6'h00;
+    	#10 $display(" Memory is read from Address %h----%h\n",Addr,DataBus);
+      	#20;
+		Addr = 6'h01;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+		#20;
+		Addr = 6'h02;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+		#20;
+		Addr = 6'h03;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+		#20;
+		Addr = 6'h04;
+		#10 $display(" Memory is read from Address %h--%h\n",Addr,DataBus);
+		#20;
+		Addr = 6'h05;
+		#10 $display(" Memory is read from Address %h--%h\n",Addr,DataBus);
+		#20;
+		Addr = 6'h06;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+    	#20;
+		Addr = 6'h07;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+    	#20;
+		Addr = 6'h08;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+    	#20;
+		Addr = 6'h09;
+		#10 $display(" Memory is read from Address %h---%h\n",Addr,DataBus);
+    	#20;
+		MemRd = 0;
+		MemWr = 1;
+		Addr = 6'h00;
+    	#10 $display("Writing data %h to memory DataBus", DataBus);
+    	#10 $stop;
+  end
+  initial 
+    begin		//waveform generation code 
+      $dumpfile("dump.vcd");//this block of code not required 
+      $dumpvars;            //for xilinx or altera tools,
+   end//but open source tools and linux tools, we should declare
+endmodule
